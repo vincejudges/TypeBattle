a54f94707d96042a36a294d7e6fac0b40e67626e
e <11612425@mail.sustech.edu.cn>
2019-08-21 11:28:24 +0800

    format the program

--- Hong/TB.py
+++ Hong/TB.py
@@ -1,4 +1,6 @@
-import pygame, sys, random
+import pygame
+import sys
+import random
 
 screen = pygame.display.set_mode((800, 600), 0, 0)
 back = pygame.image.load("bk.jpg")
@@ -6,47 +8,50 @@ word = []
 nowpos = 0
 score = 0
 
+
 def init():
-	pygame.init()
-	pygame.display.set_caption("TypeBattle!")
-	file = open('file.txt', 'r')
-	lines = file.readlines()
-	for str in lines:
-		for ch in str:
-			if ch != '\n':
-				word.append(ord(ch))
+    pygame.init()
+    pygame.display.set_caption("TypeBattle!")
+    file = open('file.txt', 'r')
+    lines = file.readlines()
+    for str in lines:
+        for ch in str:
+            if ch != '\n':
+                word.append(ord(ch))
 
 
 def loop():
-	while True:
-		screen.blit(back, (0, 0))
-		action()
-		pygame.time.delay(1)
-		pygame.display.update()
+    while True:
+        screen.blit(back, (0, 0))
+        action()
+        pygame.time.delay(1)
+        pygame.display.update()
+
 
 def action():
-	global score
-	global nowpos
-	global word
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			sys.exit()
-		if event.type == pygame.KEYDOWN:
-			if nowpos == len(word):
-				print("Goooooooooood!")
-				continue
-			if event.key == word[nowpos]:
-				score += 1
-				nowpos += 1
-	printScore()
-	
+    global score
+    global nowpos
+    global word
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if nowpos == len(word):
+                print("Goooooooooood!")
+                continue
+            if event.key == word[nowpos]:
+                score += 1
+                nowpos += 1
+    printScore()
+
+
 def printScore():
-	pygame.font.init()
-	font = pygame.font.Font("tahomabd.ttf", 16)
-	scoreShow = font.render("score:%s"%score,True,(255,0,0))
-	screen.blit(scoreShow, (20,20))
+    pygame.font.init()
+    font = pygame.font.Font("tahomabd.ttf", 16)
+    scoreShow = font.render("score:%s" % score, True, (255, 0, 0))
+    screen.blit(scoreShow, (20, 20))
 
 
 if __name__ == '__main__':
-	init()
-	loop()
+    init()
+    loop()
